46672b613de4985ee9c882bdbe8d9d3f9d50a1de
Boris Mwnaga <borismwanga@gmail.com>
2023-07-01 18:07:44 +0200

    retrive url / change short to regular

--- background.js
+++ background.js
@@ -8,3 +8,10 @@ chrome.action.onClicked.addListener((tab) => {
   function changeBackground() {
     document.body.style.backgroundColor = 'green';
 }
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  chrome.storage.local.get('isExtensionOn', function(data) {
+    if (data.isExtensionOn) {
+      chrome.tabs.update(request.tabId, { url: request.newUrl });
+    }
+  });
+});
--- popup.js
+++ popup.js
@@ -1,8 +1,27 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    let url = tabs[0].url;
-    document.getElementById('url').textContent = url;
-
-    console.log(url);
-    
+    let url = tabs[0].url; // retrieve url
+    if (url.includes("https://www.youtube.com/shorts/")) {
+    // spilt video code to ytb url
+      const vid = url.split("https://www.youtube.com/shorts/")
+      chrome.runtime.sendMessage({
+        message: "update_tab_url",
+        tabId: tabs[0].id,
+        newUrl: "https://www.youtube.com/watch?v=" + `${vid[1]}`
+      });
+    } else {
+      document.getElementById('url').textContent = url;
+    }
+  });
+  
+  document.getElementById('turnOn').addEventListener('click', function() {
+    chrome.storage.local.set({ 'isExtensionOn': true }, function() {
+      console.log('The extension is on.');
+    });
+  });
+  
+  document.getElementById('turnOff').addEventListener('click', function() {
+    chrome.storage.local.set({ 'isExtensionOn': false }, function() {
+      console.log('The extension is off.');
+    });
   });
   
