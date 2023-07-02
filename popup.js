3d7abafafe65ca9bf2a892d65884fd1fcd30a3ee
Boris Mwnaga <borismwanga@gmail.com>
2023-07-02 13:26:36 +0200

    design && colors

--- popup.js
+++ popup.js
@@ -2,13 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.checkbox').addEventListener('change', function(event) {
         let isExtensionOn = event.target.checked;
         chrome.storage.local.set({ 'isExtensionOn': isExtensionOn }, function() {
-          console.log('The extension is', isExtensionOn ? 'on' : 'off');
+            console.log('The extension is', isExtensionOn ? 'on' : 'off');
         });
-      });
-      
-      // On popup open, ensure the checkbox is in the correct state
-      chrome.storage.local.get('isExtensionOn', function(data) {
+    });
+
+    // On popup open, ensure the checkbox is in the correct state
+    chrome.storage.local.get('isExtensionOn', function(data) {
         document.querySelector('.checkbox').checked = !!data.isExtensionOn;
-      });
-  });
-  
+    });
+
+    // Event listener for the refresh button
+    document.querySelector('#refresh-button').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.reload(tabs[0].id);
+        });
+    });
+
+});
+
